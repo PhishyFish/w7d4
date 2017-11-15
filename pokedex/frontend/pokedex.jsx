c1192ad62ed4df253eb208a819a4e654a90a3cec
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-11-15 15:51:01 -0800

    Added API util, action creators, reducers, store, and thunk middleware

---
+++ pokedex/frontend/pokedex.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {receiveAllPokemon, requestAllPokemon} from './actions/pokemon_actions';
+import {fetchAllPokemon} from './util/api_util';
+import configureStore from './store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  window.store = store;
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  window.requestAllPokemon = requestAllPokemon;
+  window.receiveAllPokemon = receiveAllPokemon;
+  window.fetchAllPokemon = fetchAllPokemon;
+
+  const rootEl = document.getElementById('root');
+  ReactDOM.render(<h1>Pokedex YAY!</h1>, rootEl);
+});
